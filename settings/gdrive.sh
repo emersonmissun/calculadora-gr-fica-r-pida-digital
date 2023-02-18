5ed1c4098ff34492a279c2a418156b5146593263
emersonmissun <emerson.missun@gmail.com>
2023-02-18 11:55:15 -0300

    first commit

---
+++ settings/gdrive.sh
@@ -0,0 +1,9 @@
+#! /bin/env bash
+
+# Variáveis de configuração do script
+# gdrive_user="grafica.refacao@gmail.com"
+# gdrive_password="u0O6x#80@}U=h{]\U"
+# gdrive_folder="gdrive_folder"
+# gdrive_file="gdrive_file"
+
+
