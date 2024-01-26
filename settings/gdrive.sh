a8157a12b5f003ffb313d08a1256960d1e98c0fe
Emerson Missun <emerson.missun@gmail.com>
2024-01-26 00:14:31 -0300

    Update gdrive.sh

--- settings/gdrive.sh
+++ settings/gdrive.sh
@@ -1,9 +1,1 @@
-#! /bin/env bash
-
-# Variáveis de configuração do script
-# gdrive_user="grafica.refacao@gmail.com"
-# gdrive_password="u0O6x#80@}U=h{]\U"
-# gdrive_folder="gdrive_folder"
-# gdrive_file="gdrive_file"
-
 
